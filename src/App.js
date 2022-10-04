8de57349f3a4c9651ee1ebc409e6c9093c74af2b
mokshi1411 <mokshi1411@gmail.com>
2022-10-04 18:41:29 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import React from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+      <Navbar/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i class='fa-solid fa-house'></i>
+      </h1>
+      <form className='search'>
+        <input type='search' name='search' placeholder='Search' />
+      </form>
+      <h1 className="ss">
+      <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
